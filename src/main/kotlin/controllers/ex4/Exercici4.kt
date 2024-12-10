00444cee38f10b1511b1f3697566fac2b3de2d88
Anabel <anabel.asensio.7e8@itb.cat>
2024-12-10 19:01:09 +0100

    Exercici 4 sense test

--- src/main/kotlin/controllers/ex4/Exercici4.kt
+++ src/main/kotlin/controllers/ex4/Exercici4.kt
@@ -1,2 +1,46 @@
 package controllers.ex4
 
+import controllers.ex2.*
+import java.util.Scanner
+
+
+fun main() {
+    var scan = abrirScanner()
+    var text = demanarFrase ("Introdueix el text que vols centrar: ", scan)
+    titol(text)
+    cerrarScanner(scan)
+}
+
+/**
+ * Funció per demanar una frase per teclat
+ * @param msg Imprimim un misatge per pantalla
+ * @param scan Introduïm una frase per teclat
+ * @return Retorna la frase introduïda
+ * @author Anabel
+ */
+fun demanarFrase(msg: String, scan: Scanner): String {
+    print(msg)
+    var frase = scan.nextLine()
+    return frase
+}
+
+/**
+ * Funció que imprimeix la frase introduïda per teclat de forma centrada
+ * @param text Frase introduïda per teclat
+ * @author Anabel
+ */
+fun titol(text: String) {
+    val amplePantalla = 80
+    if (text.length > amplePantalla) {
+        println(text)
+    } else {
+        val espais = (amplePantalla - text.length) / 2
+        var espaisBuit = ""
+
+        for (i in 1..espais) {
+            espaisBuit += " "
+        }
+        println(espaisBuit + text)
+    }
+}
+
---
+++ src/test/kotlin/controllers/ex4/Exercici4KtTest.kt
@@ -0,0 +1,21 @@
+package controllers.ex4
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Exercici4KtTest {
+
+    @Test
+    fun provaTitol1() {
+
+    }
+    @Test
+    fun provaTitol2() {
+
+    }
+    @Test
+    fun provaTitol3() {
+
+    }
+}
