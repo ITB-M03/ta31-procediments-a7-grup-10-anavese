be94d8c356f83668508ff1e07dcfe1855d48a007
CataGenovese <catalina.genovese.7e8@itb.cat>
2024-12-10 13:52:49 +0100

    .

--- src/main/kotlin/controllers/ex1/Exercici1.kt
+++ src/main/kotlin/controllers/ex1/Exercici1.kt
@@ -1,5 +1,33 @@
 package org.example.controllers
+import java.util.*
+
 
 fun main() {
-    //TODO
+    val sc: Scanner = obrimScanner()
+
+    // Demanem la data
+    var day = sc.nextInt()
+    print("Escriu el dia de la compra: ")
+    var month = sc.nextInt()
+    print("Escriu el mes de la compra: ")
+    var year = sc.nextInt()
+    print("Escriu l'any de la compra: ")
+
 }
+fun obrimScanner(): Scanner {
+    return Scanner(System.`in`)
+}
+
+/**
+ * Funció per llegir els números
+ */
+fun data(sc:Scanner, msg: String, dia:Int, mes:Int, any:Int): String {
+    print(msg)
+    var data=Calendar.getInstance()
+    var ndia= data.get(Calendar.DAY_OF_YEAR)
+    var nmes= data.get(Calendar.MONTH)
+    var nany= data.get(Calendar.YEAR)
+
+    return "$ndia-$nmes-$nany"
+}
+
