3d00e18ac5424fcc1c31b4811e9f86fd22bf257a
Catalina <catalina.genovese.7e8@itb.cat>
2024-12-16 10:47:42 +0100

    ex1

--- src/test/kotlin/controllers/ex1/Exercici1KtTest.kt
+++ src/test/kotlin/controllers/ex1/Exercici1KtTest.kt
@@ -3,6 +3,25 @@ package controllers.ex1
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
- class Exercici1KtTest {
+import kotlin.test.Test
+
+class Exercici1KtTest {
 
 //preu: no cal fer testos ja que
+class Exercici1KtTest {
+  @kotlin.test.Test
+  fun demanemPreuFloatNormal() {
+    var expected= 200.21
+      assertEquals(expected, 200.21)
+  }
+  @kotlin.test.Test
+  fun demanemPreuAmbValor0() {
+    var expected= 0
+      assertEquals(expected, 0)
+  }
+  @kotlin.test.Test
+  fun demanemPreuSenseFloat() {
+    var expected= 200.0
+      assertEquals(expected, 200.0)
+  }
+ }
